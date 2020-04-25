75b71a49fd3000b2567151142048e4c01cf35878
Nihilus119 <ritter.johannes.ma@gmail.com>
2020-04-25 20:48:02 +0200

    Tabelle Artikel und Detail

--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artikel(models.Model):
+    artikelnr = models.IntegerField(primary_key=True, auto_created=True)
+    artikelname = models.CharField(max_length=50)
+    preis = models.FloatField()
+    menge = models.IntegerField()
--- webshop/shop/views.py
+++ webshop/shop/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html')
+
+
+def detail(request, artikel=1):
+    context = {
+        'artikelnr': artikel
+    }
+    return render(request, 'detail.html', context=context)
