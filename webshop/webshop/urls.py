e18a976a5553694f9340dc9c3d97956100406908
Nihilus119 <ritter.johannes.ma@gmail.com>
2020-04-25 20:08:50 +0200

    Shop Index

--- webshop/webshop/urls.py
+++ webshop/webshop/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('nutzer/', include('nutzer.urls'))
+    path('nutzer/', include('nutzer.urls')),
+    path('', include('shop.urls'))
 ]
