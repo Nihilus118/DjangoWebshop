211b1649fa0522814c5e77eaa0f667ae74e2cdfb
Nihilus119 <ritter.johannes.ma@gmail.com>
2020-04-25 21:31:03 +0200

    Tabellen Farben, Kaesten, Bestellungen

--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Kaesten(models.Model):
+    beschreibung = models.CharField(max_length=150)
+    groesse = models.CharField(max_length=50)
+    verfuegbar = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.beschreibung
+
+
+class Farben(models.Model):
+    name = models.CharField(max_length=30)
+    verfuegbar = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Artikel(models.Model):
@@ -8,3 +24,32 @@ class Artikel(models.Model):
     artikelname = models.CharField(max_length=50)
     preis = models.FloatField()
     menge = models.IntegerField()
+    groesse_kasten = models.ForeignKey(
+        Kaesten,
+        default=1,
+        on_delete=models.DO_NOTHING
+    )
+    farbe_kasten = models.ForeignKey(
+        Farben,
+        default=1,
+        on_delete=models.DO_NOTHING
+    )
+
+    def __str__(self):
+        return self.artikelname
+
+
+class Bestellungen(models.Model):
+    bestellnummer = models.IntegerField(primary_key=True, auto_created=True)
+    kundennummer = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    bestelldatum = models.DateTimeField(auto_now_add=True)
+    stadt = models.CharField(max_length=30)
+    strasse = models.CharField(max_length=50)
+    hausnummer = models.CharField(max_length=5)
+    bezahlt = models.BooleanField(default=True)
+    zahldatum = models.DateTimeField()
+    kasten = models.ForeignKey(Kaesten, on_delete=models.DO_NOTHING)
+    farbe = models.ForeignKey(Farben, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.bestellnummer
