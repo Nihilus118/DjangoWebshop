f083004a98fcf4f07468eae12fa7ea6cd1a28092
Nihilus119 <ritter.johannes.ma@gmail.com>
2020-04-29 19:07:05 +0200

    Alle Artikel

--- webshop/shop/urls.py
+++ webshop/shop/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('artikel/', views.artikel, name='artikel'),
     path('detail/<int:artikel>', views.detail, name='detail'),
 ]
--- webshop/shop/views.py
+++ webshop/shop/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from .models import Artikel
 
 
 def index(request):
@@ -10,3 +12,10 @@ def detail(request, artikel=1):
         'artikelnr': artikel
     }
     return render(request, 'detail.html', context=context)
+
+
+def artikel(request):
+    context = {
+        'artikel_liste': Artikel.objects.all()
+    }
+    return render(request, 'artikel.html', context=context)
